524533f5dd14c93989c44c3e3196fe5360160bd4
Shreya Gupta <shreyagupta3011@gmail.com>
2019-07-15 12:16:37 +0530

    Update app.py

--- splitwise/app.py
+++ splitwise/app.py
@@ -5,16 +5,18 @@ import config as Config
 app = Flask(__name__)
 app.secret_key = "test_secret_key"
 
+
 @app.route("/")
 def home():
     if 'access_token' in session:
         return redirect(url_for("friends"))
     return render_template("home.html")
 
+
 @app.route("/login")
 def login():
 
-    sObj = Splitwise(Config.consumer_key,Config.consumer_secret)
+    sObj = Splitwise(Config.consumer_key, Config.consumer_secret)
     url, secret = sObj.getAuthorizeURL()
     session['secret'] = secret
     return redirect(url)
@@ -29,8 +31,8 @@ def authorize():
     oauth_token    = request.args.get('oauth_token')
     oauth_verifier = request.args.get('oauth_verifier')
 
-    sObj = Splitwise(Config.consumer_key,Config.consumer_secret)
-    access_token = sObj.getAccessToken(oauth_token,session['secret'],oauth_verifier)
+    sObj = Splitwise(Config.consumer_key, Config.consumer_secret)
+    access_token = sObj.getAccessToken(oauth_token, session['secret'], oauth_verifier)
     session['access_token'] = access_token
 
     return redirect(url_for("friends"))
@@ -41,14 +43,12 @@ def friends():
     if 'access_token' not in session:
         return redirect(url_for("home"))
 
-    sObj = Splitwise(Config.consumer_key,Config.consumer_secret)
+    sObj = Splitwise(Config.consumer_key, Config.consumer_secret)
     sObj.setAccessToken(session['access_token'])
 
     friends = sObj.getFriends()
-    return render_template("friends.html",friends=friends)
-
-
+    return render_template("friends.html", friends=friends)
 
 
 if __name__ == "__main__":
-    app.run(threaded=True,debug=True)
+    app.run(threaded=True, debug=True)
