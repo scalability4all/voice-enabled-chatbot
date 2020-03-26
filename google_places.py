5e791de999507db3b68228dbe5591a13b6b87dec
Vishi13 <vishi13jain@gmail.com>
2020-03-26 23:49:01 +0530

    added datetime changes

--- google_places.py
+++ google_places.py
@@ -37,10 +37,9 @@ def nearby_places(
 
 
 def get_location():
-    # {u'city': u'Hyderabad', u'longitude': 78.4744,
+    # {u'city': u'Hyderabad', u'longitude':78.4744,
     # u'latitude': 17.3753, u'state': u'Telangana', u'IPv4': u'157.48.48.45',
     # u'country_code': u'IN', u'country_name': u'India', u'postal': u'500025'}
-    
     url = config.geolocation_api
     response = requests.get(url)
     data = response.json()
