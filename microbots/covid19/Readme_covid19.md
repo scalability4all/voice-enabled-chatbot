d40c93607ed052f38b3eb52d6735069eeee5754c
Karina Condeixa <18695019+KC2016@users.noreply.github.com>
2020-05-18 18:40:47 +0200

    create a folder covid19 with api and readme (#56)
    * create a folder covid19 with api and readme
    
    retrieve  free api, compress and save,  with its readme.
    
    * includes mac extension in .gitignore
    
    includes .DS_Store
    
    * include new libraries in the requirements.txt
    
    includes the libraries used in covid19
    
    * moves the covid19 folder to microbots
    
    * insert  space at the last line of covid19_summary.py
    
    * create config.py and requirements.txt files inside covid-19 file
    
    * fix pep8 of the files in the covid19 folder
    
    fix the files readme_covid19.md, config.py and covid19_summary.py

---
+++ microbots/covid19/Readme_covid19.md
@@ -0,0 +1,4 @@
+# COVID-19
+
+## API: A summary of new and total cases per country updated daily.
+Using a free API from https://api.covid19api.com/summary
---
+++ microbots/covid19/covid19_summary.py
@@ -0,0 +1,32 @@
+import urllib.request
+import os.path
+import zipfile
+import pathlib
+import json
+
+print('COVID-19: a summary of new and total cases per country updated daily.')
+
+# Create a directory if it does not exist
+filesDir = 'files'
+if not os.path.exists(filesDir):
+    os.makedirs(filesDir)
+
+# Defines functions and classes which help in opening URLs (mostly HTTP) in a complex world
+# Retrieves the contents of url and places it in filename.
+endPointUrl = 'https://api.covid19api.com/summary'
+jsonFileName = 'summary.json'
+zipFileName = 'summary.zip'
+
+# relativeJsonFilePath = os.path.join(filesPath, jsonFileName)
+relativeZipFilePath = os.path.join(filesDir, zipFileName)
+
+# Request and write Json file
+urllib.request.urlretrieve(endPointUrl, jsonFileName)
+
+# Create a ziped file of files in file folder and name it with the date
+zipObj = zipfile.ZipFile(relativeZipFilePath, mode='w')
+zipObj.write(jsonFileName)
+zipObj.close()
+
+# Delete json file
+os.remove(jsonFileName)
